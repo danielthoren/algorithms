fda3f47f1c37042d6da35d7a2558d6f1dd1399fa
daniel5908 <danth343@student.liu.se>
2019-01-29 16:22:58 +0100

    Changed data representation for intervals to struct

--- labs/lab1/ass1/program2.cc
+++ labs/lab1/ass1/program2.cc
@@ -13,6 +13,12 @@ using namespace std;
 
 using namespace std;
 
+struct Interval
+{
+    double first, second;
+    int orig_index;
+};
+
 vector<int> cover(pair<double, double>& target,
 		  vector<pair<double, double>>& intervals)
 {
@@ -20,61 +26,62 @@ vector<int> cover(pair<double, double>& target,
     if (intervals.size() == 0)
 	return {};
     
-    vector<vector<double>> intervals_copy;
+    vector<Interval> intervals_copy;
     for (int i = 0; i < (int) intervals.size(); i++)
     {
 	intervals_copy.push_back(
-	    vector<double>{intervals[i].first, intervals[i].second, (double) i});
+	    Interval{intervals[i].first, intervals[i].second, i});
     }
 
     sort(intervals_copy.begin(), intervals_copy.end(),
-	 [] (vector<double>& vec1, vector<double>& vec2)
+	 [] (Interval& i1, Interval& i2)
 	 {
-	     if (vec1[0] == vec2[0])
-		 return vec1[1] > vec2[1];
-	     return vec1[0] < vec2[0];
+	     if (i1.first == i2.first)
+		 return i1.second > i2.second;
+	     return i1.first < i2.first;
 	 });
 
     double position{target.first};
-    vector<double> best{};
+    Interval best{};
+    best.orig_index = -1;
     vector<int> result{};
     int index = 0;
 
-    if (intervals_copy[0][0] > target.first)
+    if (intervals_copy[0].first > target.first)
 	return {};
 
     do
     {
 	for (unsigned int i = index;
-	     i < intervals_copy.size() && intervals_copy[i][0] <= position;
+	     i < intervals_copy.size() && intervals_copy[i].first <= position;
 	     i++)
 	{
-	    if (best.size() == 0 || (intervals_copy[i][1] > position &&
-	       (intervals_copy[i][1] - position) > (best[1] - position)))
+	    if (best.orig_index == -1 || (intervals_copy[i].second > position &&
+	       (intervals_copy[i].second - position) > (best.second - position)))
 	    {
 		best = intervals_copy[i];
 		index = i;
 
-		if (best[1] >= target.second)
+		if (best.second >= target.second)
 		{
-		    result.push_back(best[2]);
+		    result.push_back(best.orig_index);
 		    return result;
-		}		    
+		}
 	    }
 	}
 
-	if (result.size() == 0 || (result.size() != 0 && best[2] != result[result.size() - 1]))
+	if (result.size() == 0 || (result.size() != 0 && best.orig_index != result[result.size() - 1]))
 	{
-	    result.push_back(best[2]);
-	    position = best[1];
+	    result.push_back(best.orig_index);
+	    position = best.second;
 	}
 	else
 	    return {};
 	
     } while (position < target.second);
 
-    if (best[1] < target.second ||
-    	(result.size() > 0 &&intervals[result[0]].first > target.first))
+    if (best.second < target.second ||
+    	(result.size() > 0 && intervals[result[0]].first > target.first))
     	return {};
 
     return result;
