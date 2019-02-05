8b6174f9cf068650b2bd9967087e53ed65e6403e
daniel5908 <danth343@student.liu.se>
2019-02-05 17:11:00 +0100

    Started work on chopping wood

---
+++ exercises/2/chopping_wood/program.cc
@@ -0,0 +1,17 @@
+
+int main()
+{
+    int length;
+    cin >> length;
+
+    vector<int> list;
+    int elem;
+    for(int i = 0; i < length; i++)
+    {
+	cin >> elem;
+	list.push_back(elem);
+    }
+
+    
+	
+}
