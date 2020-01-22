fd993d7a2bfc19e6b3ada19c0308ca344e3946a5
daniel5908 <danth343@student.liu.se>
2020-01-22 13:37:33 +0100

    Formatted code for fenwick tree

--- labs/lab1/fenwick_tree/program.cc
+++ labs/lab1/fenwick_tree/program.cc
@@ -1,29 +1,41 @@
 #include <vector>
 #include <iostream>
 
-
+/*
+ * Fenwick tree implementation, template type must be of integer type
+ */
 template <typename T>
-void update(T index, T val, std::vector<T>& tree)
+class FenwickTree
 {
-    index += 1;
-    while (index < tree.size())
-    {
-	tree.at(index) += val;
-	index += index & (-index);
-    }
-}
+public:
 
-template <typename T>
-T query(T index, std::vector<T>& tree)
-{
-    T res = 0;
-    while (index > 0)
-    {
-	res += tree.at(index);
-	index -= index & (-index);
-    }
-    return res;
-}
+    FenwickTree(long unsigned int size) : tree(size + 1)
+	{}	
+    
+    void update(T index, T val)
+	{
+	    index += 1;
+	    while (index < tree.size())
+	    {
+		tree.at(index) += val;
+		index += index & (-index);
+	    }
+	}
+
+    T query(T index)
+	{
+	    T res = 0;
+	    while (index > 0)
+	    {
+		res += tree.at(index);
+		index -= index & (-index);
+	    }
+	    return res;
+	}
+
+private:
+    std::vector<T> tree;
+};
 
 int main()
 {
@@ -33,7 +45,7 @@ int main()
     scanf(" %ld", &arr_length);
     scanf(" %ld", &num_ops);
     
-    std::vector<long> tree(arr_length + 1);
+    FenwickTree<long> tree(arr_length);
     long index, num, res;
     for (int i = 0; i < num_ops; i++)
     {
@@ -45,13 +57,13 @@ int main()
 	    scanf(" %ld", &index);
 	    scanf(" %ld", &num);
 	    
-	    update<long>(index, num, tree);
+	    tree.update(index, num);
 	}
 	else
 	{
 	    scanf(" %ld", &index);
 	
-	    res = query<long>(index, tree);
+	    res = tree.query(index);
 
 	    printf("%ld\n", res);
 	}
