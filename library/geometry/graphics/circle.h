b62efbd19eb0f0a37f119e0e450b6e4c331bde08
daniel5908 <danne_thoren456@hotmail.com>
2020-05-04 18:02:18 +0200

    graphics: Started to experiment with sfml

--- library/geometry/graphics/circle.h
+++ library/geometry/graphics/circle.h
@@ -1,10 +1,30 @@
 
-namespace g
-{
+#include "../point.h"
+#include "graphics.h"
 
-    class Circle
+namespace dg
+{
+    template <typename T>
+    class Point
     {
-	
+    public:
+	Point(T x, T y) : pt(x, y)
+	    {}
+
+	Point(dalg::Point const& pt) : pt{pt}
+	    {};
+
+	point(Point const& other) : pt{other.pt}
+	    {}
+
+	void draw(Graphics& g)
+	    {
+		
+	    }
+
+    private:
+
+	dalg::Point<T> pt;
     };
 
 }
